d33410ddaae56b24aa18c2738cb52f254079206e
Chris Tsantiris <chris.tsantiris@gmail.com>
2023-11-28 19:01:44 -0500

    Move models to separate file

---
+++ todos.go
@@ -0,0 +1,129 @@
+package todo
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/alexeyco/simpletable"
+)
+
+func (t *Todos) AddItem(task string) {
+	todo := Todo{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+
+	*t = append(*t, todo) // Append newly created todo item to the list of To Dos
+}
+
+func (t *Todos) CompleteItem(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New("invalid index")
+	}
+
+	ls[index-1].CompletedAt = time.Now()
+	ls[index-1].Done = true
+
+	return nil
+}
+
+func (t *Todos) DeleteItem(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New("invalid index")
+	}
+
+	*t = append(ls[:index-1], ls[index:]...) // Remove the item at this index from list
+
+	return nil
+}
+
+func (t *Todos) LoadItems(filename string) error { // Load items from json file
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return err
+	}
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Todos) StoreAddedItem(filename string) error { // Write added todo back to json file
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(filename, data, 0644)
+}
+
+func (t *Todos) PrintToDos() {
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Task"},
+			{Align: simpletable.AlignCenter, Text: "Done?"},
+			{Align: simpletable.AlignRight, Text: "Created At"},
+			{Align: simpletable.AlignRight, Text: "Completed At"},
+		},
+	}
+
+	var cells [][]*simpletable.Cell
+	for i, item := range *t {
+		i++ // increment before looping because cli uses 1 as first index
+		task := blue(item.Task)
+		done := blue("No")
+		if item.Done {
+			task = green(fmt.Sprintf("\u2705 %s", item.Task))
+			done = green("Yes")
+		}
+
+		completedAtStr := item.CompletedAt.Format(time.RFC822)
+		if item.CompletedAt.IsZero() {
+			completedAtStr = ""
+		}
+		cells = append(cells, []*simpletable.Cell{
+			{Text: fmt.Sprintf("%d", i)},
+			{Text: task},
+			{Text: done},
+			{Text: item.CreatedAt.Format(time.RFC822)},
+			{Text: completedAtStr},
+		})
+	}
+	table.Body = &simpletable.Body{Cells: cells}
+	table.Footer = &simpletable.Footer{Cells: []*simpletable.Cell{
+		{Align: simpletable.AlignCenter, Span: 5, Text: red(fmt.Sprintf("You have %d pending todos", t.CountPending()))},
+	}}
+
+	table.SetStyle(simpletable.StyleUnicode)
+	table.Println()
+}
+
+func (t *Todos) CountPending() int {
+	total := 0
+	for _, item := range *t {
+		if !item.Done {
+			total++
+		}
+	}
+	return total
+}
